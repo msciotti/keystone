14b5df13a2a25aa4d0af284dcb13b2decde5cbde
Mason Sciotti <mason.sciotti@gmail.com>
2018-01-18 12:54:44 -0800

    Caching shtuff

--- plugins/helpers.py
+++ plugins/helpers.py
@@ -1,49 +1,55 @@
 import requests
-import datetime
+from datetime import datetime, timedelta
 import json
 from collections import defaultdict
 from disco.types.message import MessageEmbed
-from threading import Timer
 import constants
 
 
 KEYS = defaultdict(dict)
 AFFIXES = {}
+# Dummy timestamp
+TUESDAY_TIMESTAMP = datetime(2015, 1, 1, 1, 00, 00, 000)
 
 
 def get_weekly_affix():
-    'Getting new data...'
-    r = requests.get(constants.AFFIX_URL, timeout=2)
-    global AFFIXES
-    AFFIXES = r.json()
-    print 'Got data:'
-    print json.dumps(AFFIXES, indent=4)
-    global KEYS
-    KEYS = defaultdict(dict)
-    cache()
+    now = datetime.utcnow()
+    if now > TUESDAY_TIMESTAMP:
+        'Getting new data...'
+        r = requests.get(constants.AFFIX_URL, timeout=2)
+        global AFFIXES
+        AFFIXES = r.json()
+        print 'Got data:'
+        print json.dumps(AFFIXES, indent=4)
+        global KEYS
+        KEYS = defaultdict(dict)
+        cache()
+
+    return AFFIXES
 
 
 def cache():
     print 'Caching...'
-    now = datetime.datetime.utcnow()
+    now = datetime.utcnow()
 
     # Get datetime of next Tuesday
-    next_tuesday = now + datetime.timedelta(days=(1-now.weekday()) % 7)
+    next_tuesday = now + timedelta(days=(1-now.weekday()) % 7)
 
     # It is Tuesday my dudes
     if now.weekday() is 1:
-        one_week = datetime.timedelta(days=7)
+        one_week = timedelta(days=7)
         next_tuesday += one_week
 
-    print next_tuesday
-    # seconds_to_wait = (next_tuesday - now).total_seconds
-    # t = Timer(seconds_to_wait, get_weekly_affix)
-    # t.start()
+    next_tuesday = next_tuesday.replace(hour=16, minute=0)
+    global TUESDAY_TIMESTAMP
+    TUESDAY_TIMESTAMP = next_tuesday
+    print 'Will reset on {}'.format(TUESDAY_TIMESTAMP)
 
 
 def generate_embed(guild_id):
     embed = MessageEmbed()
     keys = ''
+    affixes = get_weekly_affix()
 
     for name, key in KEYS[guild_id].items():
         for dungeon, level in key.items():
@@ -52,7 +58,7 @@ def generate_embed(guild_id):
     if keys:
         embed.add_field(name='Current Keys', value=keys)
 
-    for affix in AFFIXES['affix_details']:
+    for affix in affixes['affix_details']:
         embed.add_field(name=affix['name'], value=affix['description'])
 
     embed.set_author(name='Mythic Keystones',
@@ -87,7 +93,7 @@ def list_keys(guild_id):
 
 
 def export_keys():
-    with open('keys.json', 'w') as key_file:
+    with open('keys', 'w') as key_file:
         json.dumps(KEYS, key_file)
     return
 
